79c315d9935118b6e3d8dc48501fb728a78bffa2
yashhkolambekar <89723372+yashhkolambekar@users.noreply.github.com>
2021-09-18 15:42:30 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -7,5 +7,12 @@ function initFun(){
         document.getElementById("status-p").innerHTML = data;
     })
 
-    
+    fetch("https://api.countapi.xyz/update/default/ff742b99-d427-449a-b96a-e5f73c49c157?amount=1")
+    .then(function(response){
+        return (response.json());
+    })
+    .then(function(json){
+        document.getElementById("pageviews").innerHTML = json.value;
+    })
+       
 }
