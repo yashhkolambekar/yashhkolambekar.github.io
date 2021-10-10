9f5f2745ff6e26d0cb05f94d68f032a21c4d5d6c
yashhkolambekar <89723372+yashhkolambekar@users.noreply.github.com>
2021-10-10 20:39:08 +0530

    Add files via upload

--- win-friends/index.js
+++ win-friends/index.js
@@ -6,9 +6,13 @@
 // }
 
 function formSubmit(){
-    document.getElementById('formOne').submit();
     var link = document.createElement('a');
     link.href = "media/Book-Summary.pdf";
     link.download = 'The Art of Influencing People.pdf';
     link.dispatchEvent(new MouseEvent('click'));
+    fetch("https://api.countapi.xyz/update/default/a465a1b1-2266-4145-8e19-bf8a6ae78388?amount=1")
+    .then(function(response){
+        return (response.json());
+    })
+    document.getElementById('formOne').submit();
 }
