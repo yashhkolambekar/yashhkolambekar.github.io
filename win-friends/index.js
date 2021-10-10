91b9c28aef2d45a05925737afaf2afc4e12b0462
yashhkolambekar <89723372+yashhkolambekar@users.noreply.github.com>
2021-10-10 19:49:46 +0530

    Add files via upload

--- win-friends/index.js
+++ win-friends/index.js
@@ -7,4 +7,8 @@
 
 function formSubmit(){
     document.getElementById('formOne').submit();
+    var link = document.createElement('a');
+    link.href = "media/Book-Summary.pdf";
+    link.download = 'The Art of Influencing People.pdf';
+    link.dispatchEvent(new MouseEvent('click'));
 }
