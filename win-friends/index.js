a39ecc462ef3eaf0a898fd680474494af2213264
yashhkolambekar <89723372+yashhkolambekar@users.noreply.github.com>
2021-10-10 19:40:16 +0530

    Add files via upload

--- win-friends/index.js
+++ win-friends/index.js
@@ -1,6 +1,10 @@
-function downloadCount(){
-fetch("https://api.countapi.xyz/update/default/a465a1b1-2266-4145-8e19-bf8a6ae78388?amount=1")
-.then(function(response){
-    return (response.json());
-})
+// function downloadCount(){
+// fetch("https://api.countapi.xyz/update/default/a465a1b1-2266-4145-8e19-bf8a6ae78388?amount=1")
+// .then(function(response){
+//     return (response.json());
+// })
+// }
+
+function formSubmit(){
+    document.getElementById('formOne').submit();
 }
