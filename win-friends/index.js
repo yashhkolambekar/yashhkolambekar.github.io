aa88988a7c74a727505c6e72b37554936af50f46
yashhkolambekar <89723372+yashhkolambekar@users.noreply.github.com>
2021-10-11 17:43:04 +0530

    Update index.js

--- win-friends/index.js
+++ win-friends/index.js
@@ -8,7 +8,7 @@
 function formSubmit(){
     var link = document.createElement('a');
     link.href = "media/Book-Summary.pdf";
-    link.download = 'The Art of Influencing People.pdf';
+    link.download = 'The Art of Handling People';
     link.dispatchEvent(new MouseEvent('click'));
     fetch("https://api.countapi.xyz/update/default/a465a1b1-2266-4145-8e19-bf8a6ae78388?amount=1")
     .then(function(response){
