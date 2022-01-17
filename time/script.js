d2e4e9cbbedf29c1ddbbe660d7925ac13cbfb85a
yashhkolambekar <89723372+yashhkolambekar@users.noreply.github.com>
2022-01-17 19:35:29 +0530

    Update script.js

--- time/script.js
+++ time/script.js
@@ -7,10 +7,10 @@ var x = setInterval(function() {
   var now = new Date().getTime();
 
   // Find the distance between now and the count down date
-  var distance = countDownDate - now;
+  // var distance = countDownDate - now;
+  var distance = now;
 
   // Time calculations for days, hours, minutes and seconds
-  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
   var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
   var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
   var seconds = Math.floor((distance % (1000 * 60)) / 1000);
