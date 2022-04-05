67583adfdd842ad466e5c5ab915b970287a0e192
yashhkolambekar <89723372+yashhkolambekar@users.noreply.github.com>
2022-04-05 18:56:24 +0530

    Update script.js

--- jee/script.js
+++ jee/script.js
@@ -1,4 +1,4 @@
-var countDownDate = new Date("April 1, 2022 00:00:00").getTime();
+var countDownDate = new Date("July 1, 2023 00:00:00").getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function() {
